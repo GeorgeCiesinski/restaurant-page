d3c6bf25c5a536f548a34cd701340b85522dfc75
George Ciesinski <george.ciesin@gmail.com>
2022-11-28 17:38:02 -0500

    Added more Webpack Configuration - Added watch script - Added HtmlWebpackPlugin - Added SCSS loader & dependencies - Created basic SCSS file - Added contentHash to output file

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({  // Creates a new index.html file
+      title: "Restaurant Page",
+      template: "./src/template.html"  // Uses basic template
+    })
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contentHash].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,  // Cleans dist folder before creating new bundle
   },
   module: {
     rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",  // 3. Inject styles into DOM
+          "css-loader",    // 2. Turn CSS into commonJS
+          "sass-loader",   // 1. Turn SASS into CSS
+        ]
+      },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
