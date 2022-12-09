e13db5d1eb23249a441feecc8bbc2feab042b69c
George Ciesinski <george.ciesin@gmail.com>
2022-12-09 18:23:40 -0500

    Initial dist subtree commit

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import "./main.scss";  // Styling
-import baseLayout from './baseLayout';
-
-// Create the base layout on page first load
-baseLayout.create();
-// Generate home page
-baseLayout.homePage();
