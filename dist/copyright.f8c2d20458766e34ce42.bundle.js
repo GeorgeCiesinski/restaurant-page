9d77eb31d34009600ba2cb49ebcd8d92d2586ad7
George Ciesinski <george.ciesin@gmail.com>
2022-12-09 18:23:40 -0500

    Initial dist subtree commit

---
+++ dist/copyright.f8c2d20458766e34ce42.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrestaurant_page=self.webpackChunkrestaurant_page||[]).push([[42],{567:(e,t,a)=>{a.d(t,{Z:()=>s});const s={generate:function(){return`Copyright &copy ${(new Date).getFullYear()} George Ciesinski <a href="https://github.com/GeorgeCiesinski"><i class="fa-brands github-link fa-github"></i></a>`}}}},e=>{e(e.s=567)}]);
